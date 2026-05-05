52e22e488bbcff1b4f10b99eba45a117a6d5ba98
Perezr12 <ST10448618@vcconnect.edu.za>
2026-05-05 20:27:06 +0200

    Fixed Github Secrets issue

--- db/entrypoint.sh
+++ db/entrypoint.sh
@@ -4,26 +4,14 @@ set -e
 /opt/mssql/bin/sqlservr &
 sql_pid=$!
 
-echo "Waiting for SQL Server..."
-
-for i in {1..60}; do
-  /opt/mssql-tools18/bin/sqlcmd \
-    -S localhost -U sa -P "$SA_PASSWORD" \
-    -C -Q "SELECT 1" >/dev/null 2>&1
-
-  if [ $? -eq 0 ]; then
-    echo "SQL Server is ready"
-    break
-  fi
-
-  echo "Waiting for database..."
+echo "Waiting for SQL Server to accept connections..."
+until sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -C -Q "SELECT 1" >/dev/null 2>&1
+do
   sleep 2
 done
 
 echo "Running init.sql..."
+sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -C -i /docker-entrypoint-initdb.d/init.sql
 
-/opt/mssql-tools18/bin/sqlcmd \
-  -S localhost -U sa -P "$SA_PASSWORD" \
-  -C -i /docker-entrypoint-initdb.d/init.sql
-
+echo "Database initialized."
 wait $sql_pid
